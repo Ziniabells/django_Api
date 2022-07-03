357f1b4920d3eedd6ae7079d80982e00bb4de6fc
Ziniabells <zainabobello@yahoo.com>
2022-07-04 00:42:21 +0100

    Link

--- links/models.py
+++ links/models.py
@@ -2,15 +2,15 @@ from django.db import models
 from django.contrib.auth import get_user_model
 from django.utils import timezone
 # Create your models here.
-class LinksApp(models.Model):
+class Link(models.Model):
     
 
 
     target_url = models.URLField(max_length=200)
-    description = models.Charfield(max_length=200)
-    slug = models.SlugField(max_length=20, blank=True, unique=True)
+    description = models.CharField(max_length=200)
+    identifier = models.SlugField(max_length=20, blank=True, unique=True)
     author = models.ForeignKey(
-        get_user_model()
+        get_user_model(),on_delete=models.CASCADE
     )
     created = models.DateTimeField(auto_now_add=True)
     active = models.BooleanField(default=True)
--- links/views.py
+++ links/views.py
@@ -1,3 +1,4 @@
 from django.shortcuts import render
-
+from rest_framework.generics import ListAPIView, CreateAPIView, RetrieveAPIView , UpdateAPIView, DestroyAPIView
 # Create your views here.
+
