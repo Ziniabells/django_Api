8be13c3c810c089532a05d8a1d97c6c798024def
Ziniabells <zainabobello@yahoo.com>
2022-07-05 18:29:32 +0100

    api

--- links/views.py
+++ links/views.py
@@ -1,4 +1,27 @@
 from django.shortcuts import render
+from .models import Link
+from .serializers import LinkSerializer
 from rest_framework.generics import ListAPIView, CreateAPIView, RetrieveAPIView , UpdateAPIView, DestroyAPIView
+
 # Create your views here.
+class LinkListApi(ListAPIView):
+    queryset = Link.objects.filter(active=True)
+    serializer_class = LinkSerializer
+
+class LinkCreateApi(CreateAPIView):
+    queryset = Link.objects.filter(active=True)
+    serializer_class = LinkSerializer
+
+class ListDetailApi(RetrieveAPIView):
+    queryset = Link.objects.filter(active=True)
+    serializer_class = LinkSerializer
+
+class LinkUpdateApi(UpdateAPIView):
+    queryset = Link.objects.filter(active=True)
+    serializer_class = LinkSerializer
+
+class LinkDeleteApi(DestroyAPIView):
+    queryset = Link.objects.filter(active=True)
+    serializer_class = LinkSerializer
+
 
