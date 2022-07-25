75302d512f016c7e4d705b05afdf6290d41c397b
Ziniabells <zainabobello@yahoo.com>
2022-07-25 18:10:30 +0100

    api

--- links/urls.py
+++ links/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
 from . import views 
 
-app_name="link"
+app_name= "link"
 
 urlpatterns = [
-    path("create/", views.LinkCreateApi.as_view(), name="api_create"),
-    path("update/<int:pk>", views.LinkUpdateApi.as_view(), name="api_update"),
-    path("delete/<int:pk>", views.LinkDeleteApi.as_view(), name="api_delete"),
-    path("", views.LinkListApi.as_view(), name="api_list"),
+    path("create/", views.PostCreateApi.as_view(), name="api_create"),
+    path("update/<int:pk>", views.PostUpdateApi.as_view(), name="api_update"),
+    path("delete/<int:pk>", views.PostDeleteApi.as_view(), name="api_delete"),
+    path("", views.PostListApi.as_view(), name="api_list"),
 ]
